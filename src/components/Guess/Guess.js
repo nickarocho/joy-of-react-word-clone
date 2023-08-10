c4b7d03b7a7aae2fd807318292ad6096e90bb69d
nicholas-arocho <n_arocho@apple.com>
2023-08-10 11:14:37 -0700

    exercise 4: Game logic

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
 
-function Guess({ value = "" }) {
+function Guess({ value = "", answer = "" }) {
+  const checkedGuess = checkGuess(value, answer);
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key={num} className="cell">
+        <span
+          key={num}
+          className={`cell ${checkedGuess && checkedGuess[num].status}`}
+        >
           {value ? value[num] : undefined}
         </span>
       ))}
