654612ab8351d6d33dc836bff3a7f4a827ef7e05
nicholas-arocho <n_arocho@apple.com>
2023-08-09 17:27:50 -0700

    exercise 1: GuessInput --solution-adjustments

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -16,7 +16,14 @@ function GuessInput() {
         type="text"
         id="guess-input"
         value={guess}
-        onChange={(e) => setGuess(e.target.value.toUpperCase())}
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        onChange={(e) => {
+          const nextGuess = e.target.value.toUpperCase();
+          setGuess(nextGuess);
+        }}
       />
     </form>
   );
