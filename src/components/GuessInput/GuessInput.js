f106f63cb9576edad5f4c20eeb12469dc3bf315f
nicholas-arocho <n_arocho@apple.com>
2023-08-09 17:15:11 -0700

    exercise 1: GuessInput

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+function GuessInput() {
+  const [guess, setGuess] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log({ guess });
+    setGuess("");
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        type="text"
+        id="guess-input"
+        value={guess}
+        onChange={(e) => setGuess(e.target.value.toUpperCase())}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
